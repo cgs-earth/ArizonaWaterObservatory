fd91d6133a1e0ebee53d8cd441f27560b48070a4
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2025-06-05 12:41:18 -0400

    init

---
+++ main.py
@@ -0,0 +1,10 @@
+# Copyright 2025 Lincoln Institute of Land Policy
+# SPDX-License-Identifier: Apache-2.0
+
+
+def main():
+    print("Hello from arizonawaterobservatory!")
+
+
+if __name__ == "__main__":
+    main()
