730b67bc4458d901492b3cd58b21f11bd74fac86
John Salman <johnfish15@gmail.com>
2025-09-24 10:33:50 -0700

    Fix format, fix lint

--- ui/src/utils/hexColor.ts
+++ ui/src/utils/hexColor.ts
@@ -5,5 +5,5 @@
 
 export const getRandomHexColor = (): string => {
   const hex = Math.floor(Math.random() * 0xffffff).toString(16);
-  return `#${hex.padStart(6, "0")}`;
+  return `#${hex.padStart(6, '0')}`;
 };
