75a6678f2a4e0faac06d6f9c990cb689ef173f61
John Salman <johnfish15@gmail.com>
2025-11-10 07:57:05 -0800

    In development message

--- ui/src/features/TopBar/Info/FAQ.tsx
+++ ui/src/features/TopBar/Info/FAQ.tsx
@@ -3,6 +3,12 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import { Group } from '@mantine/core';
+
 export const FAQ: React.FC = () => {
-  return <></>;
+  return (
+    <Group justify="center" align="center" mt={16}>
+      This tab is in development.
+    </Group>
+  );
 };
