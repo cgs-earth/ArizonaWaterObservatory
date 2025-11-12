caaadb199978182290122f853ce4695ad19e6648
Colton Loftus <70598503+C-Loftus@users.noreply.github.com>
2025-11-12 09:42:52 -0500

    simplify command

--- terraform/main.tf
+++ terraform/main.tf
@@ -60,10 +60,6 @@ resource "google_cloud_run_v2_job" "groundwatersqldumpjob" {
           value = "/cloudsql/${google_sql_database_instance.postgis.connection_name}"
         }
         env {
-          name  = "PGDATABASE"
-          value = var.POSTGRES_DB
-        }
-        env {
           name  = "PGUSER"
           value = var.POSTGRES_USER
         }
@@ -88,12 +84,12 @@ resource "google_cloud_run_v2_job" "groundwatersqldumpjob" {
             # as such we need to create it manually; we fetch the sql from the pygeoapi repo since this is the file that is used
             # to create the jobs table in the test
             curl --fail --show-error --silent https://raw.githubusercontent.com/geopython/pygeoapi/refs/heads/master/tests/data/postgres_manager_full_structure.backup.sql -o create_jobs_table.sql
-            psql -h /cloudsql/${google_sql_database_instance.postgis.connection_name} -U ${var.POSTGRES_USER} -d postgres -f create_jobs_table.sql
+            psql -d postgres -f create_jobs_table.sql
 
             # we need to wipe the database before restoring since you can't restore a dump over an existing database trivially
-            psql -h /cloudsql/${google_sql_database_instance.postgis.connection_name} -U ${var.POSTGRES_USER} -d postgres -c "DROP DATABASE IF EXISTS ${var.POSTGRES_DB};"
-            psql -h /cloudsql/${google_sql_database_instance.postgis.connection_name} -U ${var.POSTGRES_USER} -d postgres -c "CREATE DATABASE ${var.POSTGRES_DB};"
-            pg_restore --host=/cloudsql/${google_sql_database_instance.postgis.connection_name} --username=${var.POSTGRES_USER} --dbname=${var.POSTGRES_DB} --verbose --exit-on-error edr_backup.dump
+            psql -d postgres -c "DROP DATABASE IF EXISTS ${var.POSTGRES_DB};"
+            psql -d postgres -c "CREATE DATABASE ${var.POSTGRES_DB};"
+            pg_restore --dbname=${var.POSTGRES_DB} --verbose --exit-on-error edr_backup.dump
           EOT
         ]
         volume_mounts {
