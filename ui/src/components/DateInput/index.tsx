f243210681912e049b89d25b4238054cbeb45ff4
Callahan Stormer <33109142+CallStormer@users.noreply.github.com>
2026-03-20 10:30:14 -0700

    UI debounce date selection (#168)

--- ui/src/components/DateInput/index.tsx
+++ ui/src/components/DateInput/index.tsx
@@ -3,6 +3,8 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+import { useEffect } from 'react';
+import debounce from 'lodash.debounce';
 import { DateInput as DateInputComponent, DateInputProps } from '@mantine/dates';
 import styles from '@/components/DateInput/DateInput.module.css';
 import { DatePreset } from '@/components/DateInput/DateInput.types';
@@ -14,16 +16,25 @@ type Props = DateInputProps & {
 };
 
 const DateInput: React.FC<Props> = (props) => {
-  const { simplePresets, ...datePickerProps } = props;
+  const { simplePresets, onChange = () => null, ...datePickerProps } = props;
 
   // Retaining this for future reference, remove when custom presets get implemented
   // const simplePresetDates = simplePresets ? getSimplePresetDates(simplePresets) : [];
+
+  const debounceOnChange = debounce(onChange, 500);
+
+  useEffect(() => {
+    return () => {
+      debounceOnChange.cancel();
+    };
+  }, []);
 
   return (
     <DateInputComponent
       classNames={{ input: styles.input }}
       size="md"
       radius={0}
+      onChange={debounceOnChange}
       valueFormat="MM/DD/YYYY"
       // presets={[...(presets ?? []), ...simplePresetDates]}
       {...datePickerProps}
