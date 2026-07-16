8e6c1ef16852ced09f0f98f251db0f4255a28157
John Salman <31483839+john-salman@users.noreply.github.com>
2026-07-16 11:59:59 -0700

    Fix: Add a max width to parameter list to prevent style issues (#244)

--- ui/src/features/Panel/Datasets/Dataset/Header.tsx
+++ ui/src/features/Panel/Datasets/Dataset/Header.tsx
@@ -4,6 +4,7 @@
  */
 
 import { Stack, Text, Tooltip } from '@mantine/core';
+import styles from '@/features/Panel/Panel.module.css';
 import { ICollection } from '@/services/edr.service';
 import { getParameterList } from '@/utils/parameters';
 import { getProvider } from '@/utils/provider';
@@ -21,12 +22,12 @@ export const Header: React.FC<Props> = (props) => {
 
   return (
     <Tooltip label="Click to show dataset details" openDelay={500}>
-      <Stack justify="center" gap={1}>
+      <Stack justify="center" gap="calc(var(--default-spacing) / 4)">
         <Text component="h3" size="lg" lineClamp={2} title={dataset.title} fw={500}>
           <strong>{provider}</strong> {dataset.title}
         </Text>
         {parameters.length > 0 && (
-          <Text size="xs" lineClamp={2}>
+          <Text size="xs" lineClamp={2} className={styles.datasetsParameterList}>
             <strong>Parameters:</strong> {getParameterList(dataset).join(', ')}
           </Text>
         )}
